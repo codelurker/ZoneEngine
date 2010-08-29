a49fa4f559c0b8e7e52d391bc18bff325af357cc
Jetstar <jetstarforever@gmail.com>
2010-08-29 16:53:08 -0400

    Working on speech and dialog.

--- speech.py
+++ speech.py
@@ -1,1 +1,66 @@
-def 
+import re
+
+#Speech banks
+#included in source for now
+greeting_postive=['Good %timeofday%, %other.name%.']
+greeting_neutral=['Hello, %other.name%.']
+greeting_negative=['%ignore%']
+
+class dummy:
+	def __init__(self,name):
+		self.name=name
+
+a = dummy('Adam')
+b = dummy('Eve')
+
+def Greeting(a,b):
+	#a = you
+	#b = them
+	if a.race==b.race:
+		if b.race=='Human':
+			if a.gender==b.gender:
+				if a.gender=='Male':
+					FindGreeting(a.Self_SocialFemaleHuman)
+				else:
+					FindGreeting(a.Self_SocialMaleHuman)
+			else:
+				if a.gender=='Male':
+					FindGreeting(a.Self_SocialFemaleHuman)
+				else:
+					FindGreeting(a.Self_SocialMaleHuman)
+		else:
+			if a.gender==b.gender:
+				if a.gender=='Male':
+					FindGreeting(a.Self_SocialFemaleElf)
+				else:
+					FindGreeting(a.Self_SocialMaleElf)
+			else:
+				if a.gender=='Male':
+					FindGreeting(a.Self_SocialFemaleElf)
+				else:
+					FindGreeting(a.Self_SocialMaleElf)
+
+def FindGreeting(value):
+	if value>=60:
+		return greeting_postive[0]
+	elif value>40 and value<60:
+		return greeting_neutral[0]
+	elif value<=40:
+		return greeting_negative[0]
+
+def RenderResponse(a,b,string):
+	temp=a.name+': '+string
+	found=re.findall('%(.*?)%', string)
+	for entry in found:
+		temp=temp.replace(entry,GetVariable(a,b,entry))
+	temp=temp.replace('%','')
+	print temp
+
+def GetVariable(a,b,entry):
+	if entry=='timeofday':
+		return 'afternoon'
+	elif entry=='other.name':
+		return b.name
+
+RenderResponse(a,b,greeting_postive[0])
+	
