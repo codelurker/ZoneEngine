b9009853ebf9e085cf4919c87fb1c84ee9ef691f
Jetstar <jetstarforever@gmail.com>
2010-09-08 17:05:50 -0400

    Basic conversations!

--- speech.py
+++ speech.py
@@ -7,6 +7,10 @@ greeting_positive=[]
 greeting_neutral=[]
 greeting_negative=[]
 
+status_positive=[]
+status_neutral=[]
+status_negative=[]
+
 class dummy:
 	def __init__(self,name,gender,race):
 		self.name=name
@@ -30,10 +34,20 @@ class Greeting(SpeechClass):
 		self.type='greeting'
 		self.question=question
 
+class Status(SpeechClass):
+	def __init__(self,line,align,question=False):
+		self.line=line
+		self.align=align
+		self.type='status'
+		self.question=question
+
 greeting_positive.append(Greeting('Good %timeofday%, %other.name%.',1))
 greeting_positive.append(Greeting('How are you, %other.name%?',1,question=True))
 greeting_neutral.append(Greeting('Hello, %other.name%.',0))
 greeting_negative.append(Greeting('%ignore%',-1))
+
+status_positive.append(Status('I\'m %status%, %other.name%.',1))
+status_neutral.append(Status('%status%, %other.name%.',1))
 
 class Conversation:
 	def __init__(self,a,b,type):
@@ -45,40 +59,43 @@ class Conversation:
 	def start(self):
 		self.done=False
 		if self.type=='greeting':
-			resp=GetGreeting(self.a,self.b)
+			position=GetPosition(self.a,self.b)
+			resp=FindGreeting(position)
 			RenderResponse(self.a,self.b,resp)
 			self.turn='b'
 		if self.done==False:
 			if resp.type=='greeting':
 				if resp.question==True:
-					print 'QUESTION'
+					position=GetPosition(self.b,self.a)
+					resp=FindStatus(position)
+					RenderResponse(self.b,self.a,resp)
 
-def GetGreeting(a,b):
+def GetPosition(a,b):
 	#a = you
 	#b = them
 	if a.race==b.race:
 		if b.race=='Human':
 			if a.gender==b.gender:
 				if a.gender=='Male':
-					return FindGreeting(a.Self_SocialFemaleHuman)
+					return a.Self_SocialFemaleHuman
 				else:
-					return FindGreeting(a.Self_SocialMaleHuman)
+					return a.Self_SocialMaleHuman
 			else:
 				if a.gender=='Male':
-					return FindGreeting(a.Self_SocialFemaleHuman)
+					return a.Self_SocialFemaleHuman
 				else:
-					return FindGreeting(a.Self_SocialMaleHuman)
+					return a.Self_SocialMaleHuman
 		else:
 			if a.gender==b.gender:
 				if a.gender=='Male':
-					return FindGreeting(a.Self_SocialFemaleElf)
+					return a.Self_SocialFemaleElf
 				else:
-					return FindGreeting(a.Self_SocialMaleElf)
+					return a.Self_SocialMaleElf
 			else:
 				if a.gender=='Male':
-					return FindGreeting(a.Self_SocialFemaleElf)
+					return a.Self_SocialFemaleElf
 				else:
-					return FindGreeting(a.Self_SocialMaleElf)
+					return a.Self_SocialMaleElf
 
 def FindGreeting(value):
 	if value>=60:
@@ -87,6 +104,14 @@ def FindGreeting(value):
 		return greeting_neutral[0]
 	elif value<=40:
 		return greeting_negative[0]
+
+def FindStatus(value):
+	if value>=60:
+		return status_positive[random.randint(0,len(status_positive)-1)]
+	elif value>40 and value<60:
+		return status_neutral[0]
+	elif value<=40:
+		return status_negative[0]
 
 def RenderResponse(a,b,string):
 	line=string.get()
@@ -102,6 +127,8 @@ def GetVariable(a,b,entry):
 		return 'afternoon'
 	elif entry=='other.name':
 		return b.name
+	elif entry=='status':
+		return 'Fine'
 
 #RenderResponse(a,b,greeting_positive[0])
 Conversation(a,b,'greeting')
