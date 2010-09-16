c39e314078177d8a3c1df481c59848494e7644c9
Jetstar <jetstarforever@gmail.com>
2010-09-16 10:04:35 -0400

    Bare-bones alife.py with a single class and some placeholders.

--- alife.py
+++ alife.py
@@ -1,1 +1,18 @@
+#!/usr/bin/env python
 #New ALife
+
+class ALife(object):
+	def __init__(self):
+		#Public range
+		self.name=['','']
+		self.gender=''	
+		self.race=''
+		self.age=0
+		self.birthplace=None
+		
+		#Stat range
+		self.strength=0
+		self.dexterity=0
+
+		#Private range
+		self._id=0
